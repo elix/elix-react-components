c35b8b74b557ecfddb9056a4839e3d689915ce4d
Jan Miksovsky <jan@quickui.org>
2017-07-20 16:32:03 -0700

    Home/End

--- src/mixins/DirectionSelectionMixin.js
+++ src/mixins/DirectionSelectionMixin.js
@@ -1,19 +1,6 @@
 export default function DirectionSelectionMixin(Base) {
   return class DirectionSelection extends Base {
 
-    goUp() {
-      this.setState((prevState, props) => {
-        const selectedIndex = prevState.selectedIndex;
-        const newIndex = Math.max(selectedIndex - 1, 0);
-        return {
-          selectedIndex: newIndex
-        };
-      });
-
-      // TODO: Return true if index moved, false otherwise.
-      return true;
-    }
-
     goDown() {
       this.setState((prevState, props) => {
         const selectedIndex = prevState.selectedIndex;
@@ -23,6 +10,49 @@ export default function DirectionSelectionMixin(Base) {
         };
       });
       
+      // TODO: Return true if index moved, false otherwise.
+      return true;
+    }
+
+    goEnd() {
+      this.setState((prevState, props) => {
+        const newIndex = props.children.length - 1;
+        return {
+          selectedIndex: newIndex
+        };
+      });
+      
+      // TODO: Return true if index moved, false otherwise.
+      return true;
+    }
+
+    // TODO
+    goLeft() {}
+
+    // TODO
+    goRight() {}
+
+    goStart() {
+      this.setState((prevState, props) => {
+        const newIndex = Math.min(0, props.children.length - 1);
+        return {
+          selectedIndex: newIndex
+        };
+      });
+      
+      // TODO: Return true if index moved, false otherwise.
+      return true;
+    }
+
+    goUp() {
+      this.setState((prevState, props) => {
+        const selectedIndex = prevState.selectedIndex;
+        const newIndex = Math.max(selectedIndex - 1, 0);
+        return {
+          selectedIndex: newIndex
+        };
+      });
+
       // TODO: Return true if index moved, false otherwise.
       return true;
     }
--- src/mixins/KeyboardDirectionMixin.js
+++ src/mixins/KeyboardDirectionMixin.js
@@ -4,14 +4,38 @@ export default function KeyboardDirectionMixin(Base) {
     keydown(event) {
       let handled = false;
 
+      // TODO
+      const horizontal = false;
+      const vertical = true;
+
+      // Ignore Left/Right keys when metaKey or altKey modifier is also pressed,
+      // as the user may be trying to navigate back or forward in the browser.
       switch (event.keyCode) {
-
-        case 38: // Up
-          handled = this.goUp();
+        case 35: // End
+          handled = this.goEnd();
           break;
-
+        case 36: // Home
+          handled = this.goStart();
+          break;
+        case 37: // Left
+          if (horizontal && !event.metaKey && !event.altKey) {
+            handled = this.goLeft();
+          }
+          break;
+        case 38: // Up
+          if (vertical) {
+            handled = event.altKey ? this.goStart() : this.goUp();
+          }
+          break;
+        case 39: // Right
+          if (horizontal && !event.metaKey && !event.altKey) {
+            handled = this.goRight();
+          }
+          break;
         case 40: // Down
-          handled = this.goDown();
+          if (vertical) {
+            handled = event.altKey ? this.goEnd() : this.goDown();
+          }
           break;
       }
 
