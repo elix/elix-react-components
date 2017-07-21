10a3c343223ab77ce1b645fcd3a3a841676576c3
Rob Bearman <robbear@hyperfine.com>
2017-07-21 15:45:44 +0000

    Add ES5 transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,10 @@ module.exports = {
 			{
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
-				loader: 'babel-loader'
+				loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
 			}
     ]
   },
