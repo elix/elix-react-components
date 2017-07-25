6e27bacaf3f52fb8ab473cbe54d403728310196d
Jan Miksovsky <jan@quickui.org>
2017-07-25 14:34:32 -0700

    Having test go down and then back up, repeatedly.

--- demos/src/testListBoxDemo.jsx
+++ demos/src/testListBoxDemo.jsx
@@ -5,11 +5,20 @@ export default function(root) {
   console.log(`testing`);
   const fixture = root.children[0];
   fixture.focus();
+  let i = 0;
+  let down = true;
   const interval = setInterval(() => {
     if (document.activeElement !== fixture) {
       // Break
       clearInterval(interval);
     }
-    ReactTestUtils.Simulate.keyDown(fixture, { keyCode : 40 });
+    const keyCode = down ? 40 : 38;
+    ReactTestUtils.Simulate.keyDown(fixture, { keyCode });
+    i += down ? 1 : -1;
+    if (down && i > 45) {
+      down = false;
+    } else if (!down && i < -5) {
+      down = true;
+    }
   }, 50);
 }
