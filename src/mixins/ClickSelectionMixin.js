5e733593345ce17b3db4e93caf993baee763a87d
Jan Miksovsky <jan@quickui.org>
2017-08-08 09:53:51 -0700

    ClickSelectionMixin only processes clicks performed with main (usually left) button.

--- src/mixins/ClickSelectionMixin.js
+++ src/mixins/ClickSelectionMixin.js
@@ -5,6 +5,11 @@ export default function ClickSelectionMixin(Base) {
   return class ClickSelection extends Base {
 
     click(event) {
+
+      // Only process events for the main (usually left) button.
+      if (event.button !== 0) {
+        return;
+      }
       const targetIndex = this.indexOfTarget(event.target);
       const selectionRequired = this.props.selectionRequired || this.defaults.selectionRequired;
       if (targetIndex >= 0 || !selectionRequired) {
